6ad83b1e05443f604c767e2becc50fcea1b64edc
dsy <deng.siyang@outlook.com>
2023-07-03 02:29:49 +0800

    completed lab02

---
+++ labs/lab02/lab02.py
@@ -0,0 +1,130 @@
+
+def composer(f, g):
+    """Return the composition function which given x, computes f(g(x)).
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> a1 = composer(square, add_one)   # (x + 1)^2
+    >>> a1(4)
+    25
+    >>> mul_three = lambda x: x * 3      # multiplies 3 to x
+    >>> a2 = composer(mul_three, a1)    # ((x + 1)^2) * 3
+    >>> a2(4)
+    75
+    >>> a2(5)
+    108
+    """
+    return lambda x: f(g(x))
+
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1)^2 == 0^2 + 1
+    True
+    >>> b1(4)                            # (4 + 1)^2 != 4^2 + 1
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda x: composer(f, g)(x) == composer(g, f)(x)
+
+
+def count_cond(condition):
+    """Returns a function with one parameter N that counts all the numbers from
+    1 to N that satisfy the two-argument predicate function Condition, where
+    the first argument for Condition is N and the second argument is the
+    number from 1 to N.
+
+    >>> count_factors = count_cond(lambda n, i: n % i == 0)
+    >>> count_factors(2)   # 1, 2
+    2
+    >>> count_factors(4)   # 1, 2, 4
+    3
+    >>> count_factors(12)  # 1, 2, 3, 4, 6, 12
+    6
+
+    >>> is_prime = lambda n, i: count_factors(i) == 2
+    >>> count_primes = count_cond(is_prime)
+    >>> count_primes(2)    # 2
+    1
+    >>> count_primes(3)    # 2, 3
+    2
+    >>> count_primes(4)    # 2, 3
+    2
+    >>> count_primes(5)    # 2, 3, 5
+    3
+    >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
+    8
+    """
+    "*** YOUR CODE HERE ***"
+    """def count(N):
+        res, k = 0, 1
+        while k <= N:
+            res, k = res + 1 if condition(N, k) else res, k + 1
+        return res
+    return count"""
+    return lambda N: sum(1 for i in range(1, N + 1) if condition(N, i))
+
+
+def multiple(a, b):
+    """Return the smallest number n that is a multiple of both a and b.
+
+    >>> multiple(3, 4)
+    12
+    >>> multiple(14, 21)
+    42
+    """
+    "*** YOUR CODE HERE ***"
+    f = lambda a, b: a if b == 0 else f(b, a % b) # 递归辗转相除发求最大公因数
+    return a * b // f(a, b)
+
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    "*** YOUR CODE HERE ***"
+    def f(n):
+        if n == 0:
+            return lambda x: x
+        elif n == 1:
+            return lambda x: f1(x)
+        elif n == 2:
+            return lambda x: f2(f1(x))
+        elif n == 3:
+            return lambda x: f3(f2(f1(x)))
+        elif n % 3 == 0:
+            return lambda x: f3(f2(f1(f(n - 3)(x))))
+        elif n % 3 == 1:
+            return lambda x: f1(f(n - 1)(x))
+        elif n % 3 == 2:
+            return lambda x: f2(f1(f(n - 1)(x)))
+    return f
